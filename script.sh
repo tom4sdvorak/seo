0d9bc8297dda6b339145b1dddf33e3c3ec780e23
toutouxc <toutou@e-79-50.eduroam.fit.cvut.cz>
2017-05-16 11:39:12 +0200

    nicer script

--- script.sh
+++ script.sh
@@ -3,7 +3,7 @@ escape=`printf "\e"`
 ruby=`ruby -v`
 val="$?"
 if [ $val -eq 127 ] ; then
-	echo "FAIL:  Ruby not installed. Please install it."
+	echo "$escape[01;32mFAIL:$escape[00m Ruby not installed. Please install it."
 	exit 1
 else
 
@@ -11,7 +11,7 @@ else
 	correct=`echo "$version >= 2.1" | bc`
  
 	if [ $correct -eq 0 ] ; then
-		echo "FAIL: Your Ruby version is too low ( >= 2.1 is needed)"
+		echo "$escape[01;32mFAIL:$escape[00m Your Ruby version is too low ( >= 2.1 is needed)"
 		exit 1
 	else
 		echo "$escape[01;32mOK:$escape[00m Ruby version is installed at version ($version) ( >= 2.1 is needed)"
@@ -22,7 +22,7 @@ gem=`gem -v`
 val="$?"
 
 if [ $val -eq 127 ] ; then
-	echo "FAIL: RubyGems not installed. Please install it."
+	echo "$escape[01;32mFAIL:$escape[00m RubyGems not installed. Please install it."
 	exit 1
 else
 	version=`gem -v`
@@ -33,7 +33,7 @@ fi
 node=`node -v` # won't work on Linux
 val="$?"
 if [ $val -eq 127 ] ; then
-	echo "FAIL: Node.js not installed. Please install it."
+	echo "$escape[01;32mFAIL:$escape[00m Node.js not installed. Please install it."
 	exit 1
 else
 	version=`node -v | tr -d 'v'`
@@ -43,7 +43,7 @@ fi
 npm=`npm -v`
 val="$?"
 if [ $val -eq 127 ] ; then
-	echo "FAIL: npm not installed. Please install it."
+	echo "$escape[01;32mFAIL:$escape[00m npm not installed. Please install it."
 	exit 1
 else
 	version=`npm -v | tr -d 'v'`
@@ -53,7 +53,7 @@ fi
 if [ $val -eq 0 ] ; then
 	echo "$escape[01;32mOK:$escape[00m The pdf-reader gem is installed."
 else
-	echo "FAIL: The pdf-reader gem is not installed. Please install it. (or RubyGems is unreachable)"
+	echo "$escape[01;32mFAIL:$escape[00m The pdf-reader gem is not installed. Please install it. (or RubyGems is unreachable)"
 	echo "Trying to install:"
 	gem install pdf-reader || exit 1
 fi
